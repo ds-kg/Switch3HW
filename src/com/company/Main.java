7f9744261e44e869b9ce02f4bab8c92891b18cea
Kenzhebaev Dastan <84958200+ds-kg@users.noreply.github.com>
2021-06-02 14:48:41 +0600

    выполнил ДЗ по Свитчу

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Dastan";
+        String result = name + " Good Morning";
+        System.out.println(result);
+
+        String name2 = "Ruslan";
+        String result2 = name2 + " Good Afternoon";
+        System.out.println(result2);
+
+        String name3 = "Meder";
+        String result3 = name3 + " Good Evening";
+        System.out.println(result3);
+
+        String name4 = "Dastan, Ruslan, Meder";
+        String result4 = name4 + " Good Day for everyone";
+        System.out.println(result4);
+
+
+
+
+    }
+}
