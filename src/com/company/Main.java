1f4dbb6c869a4be38e3fe816c51c804d0b3fc7e7
Kenzhebaev Dastan <84958200+ds-kg@users.noreply.github.com>
2021-06-02 15:16:17 +0600

    Исправлено

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,39 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-        String name = "Dastan";
-        String result = name + " Good Morning";
-        System.out.println(result);
+    public static void main (String[] args) {
 
-        String name2 = "Ruslan";
-        String result2 = name2 + " Good Afternoon";
-        System.out.println(result2);
+        String[] names = new String[3];
+        names[0] = "Dastan,";
+        names[1] = "Meder,";
+        names[2] = "Aktan,";
+        System.err.println (Arrays.toString(names));
 
-        String name3 = "Meder";
-        String result3 = name3 + " Good Evening";
-        System.out.println(result3);
+        String[] names3 = new String[names.length+1];
+        String name4="Dastan, Meder, Aktan!,";
+        int in=0;
+        for (String n:names) {
+            names3[in]=n; in++;
+        }
+        names3[in] = name4;
+        System.out.println(Arrays.toString(names3));
+        names = names3;
 
-        String name4 = "Dastan, Ruslan, Meder";
-        String result4 = name4 + " Good Day for everyone";
-        System.out.println(result4);
 
+        for (int i = 0; i < names.length; i++) {
+            switch (i) {
+                case 0 -> System.err.println(names[0] + " Goog Morning!");
+                case 1 -> System.err.println(names[1] + " Goog Afternoon!");
+                case 2 -> System.err.println(names[2] + " Goog Evening!");
+                case 3 -> System.err.println(names[3] + " Салам Алейкум! Всем вам!");
+            }
 
+        }
 
 
     }
+
 }
